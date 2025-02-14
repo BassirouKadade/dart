cf7e0740fd20824c79a7872480f522d96f57ec8f
Baskad <bassiroukadade1@gmail.com>
2025-02-14 22:49:33 +0100

    premier commit

---
+++ bin/projet_dart.dart
@@ -0,0 +1,9 @@
+
+int Function(int i)  somme(int n) {
+  return (int i) => i * n;
+}
+
+void main() {
+  int Function(int i) x = somme(3);
+  print(x(9));
+}
---
+++ test/projet_dart_test.dart
@@ -0,0 +1,8 @@
+import 'package:projet_dart/projet_dart.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
