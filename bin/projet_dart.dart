48e9baf47664a7e8437af7fc98e7beddf17bae8b
Baskad <bassiroukadade1@gmail.com>
2025-02-15 16:10:15 +0100

    ajout de la fonction somme

--- bin/projet_dart.dart
+++ bin/projet_dart.dart
@@ -1,6 +1,14 @@
+int somme(){
+  int a = 1;
+  int b = 2;
+  return a + b;
+}
+
 void main() {
   print("Hello from the Dart app!");
   print("Hello from the Dart app! baskad");
   // MESSAGE
   print("je suis bassirou kadade ...");
+  print(somme());
+  
 }
