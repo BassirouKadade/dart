54fbb21c652de165529f69e86c91ebbef61d44a5
Baskad <bassiroukadade1@gmail.com>
2025-02-15 16:04:04 +0100

    tets

--- bin/projet_dart.dart
+++ bin/projet_dart.dart
@@ -1,16 +1,6 @@
-int Function(int i) somme(int n) {
-  return (int i) => i * n;
-}
-
-int Function(int i) carre(int n) {
-  return (int i) => i * i;
-}
-
-List<int> tabs = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-
 void main() {
-  int Function(int i) x = somme(3);
-  print("oui");
-  print("je suis basssioru");
-
+  print("Hello from the Dart app!");
+  print("Hello from the Dart app! baskad");
+  // MESSAGE
+  print("je suis bassirou kadade ...");
 }
