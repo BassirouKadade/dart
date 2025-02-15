8966f3c848434e938653df5e787014e9c8b1b47e
Baskad <bassiroukadade1@gmail.com>
2025-02-15 14:48:00 +0100

    ajout de la fonction carre

--- bin/projet_dart.dart
+++ bin/projet_dart.dart
@@ -2,10 +2,15 @@ int Function(int i) somme(int n) {
   return (int i) => i * n;
 }
 
+int Function(int i) carre(int n) {
+  return (int i) => i * i;
+}
+
 List<int> tabs = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
 void main() {
   int Function(int i) x = somme(3);
   print("oui");
   print("je suis basssioru");
+
 }
