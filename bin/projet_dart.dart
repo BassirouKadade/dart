34627025cbf9860504c059be1d512fc28fe583db
Baskad <bassiroukadade1@gmail.com>
2025-02-15 13:49:46 +0100

    test diff

--- bin/projet_dart.dart
+++ bin/projet_dart.dart
@@ -1,11 +1,10 @@
-int Function(int i)  somme(int n) {
+int Function(int i) somme(int n) {
   return (int i) => i * n;
 }
 
 List<int> tabs = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-
 void main() {
   int Function(int i) x = somme(3);
-  print(x(9));
+  print("oui");
 }
